72cb78ae009c59f710469f10ae457686c90cc163
Umiacha <umiacha@yandex.ru>
2026-08-22 21:05:35 +0300

    Add minimal admin.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Announcements
+
+
+@admin.register(Announcements)
+class AnnouncementAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'published_at',
+        'is_published',
+        'updated_at',
+    )
+    list_filter = (
+        'is_published',
+        'published_at',
+    )
+    search_fields = (
+        'title',
+        'text',
+    )
+    ordering = (
+        '-published_at',
+    )
+    list_editable = (
+        'is_published',
+    )
+    readonly_fields = (
+        'created_at',
+        'updated_at',
+    )
+
+
+admin.site.site_header = 'Администрирование сайта ЛХВЭ'
+admin.site.title = 'ЛХВЭ'
+admin.site.index_title = 'Управление сайтом'
--- core/models.py
+++ core/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Announcements(models.Model):
+    title = models.CharField(
+        'Заголовок',
+        max_length=200,
+    )
+    text = models.TextField(
+        'Текст',
+    )
+    is_published = models.BooleanField(
+        'Опубликовано',
+        default=False,
+    )
+    published_at = models.DateTimeField(
+        'Дата публикации',
+        default=timezone.now()
+    )
+    created_at = models.DateTimeField(
+        'Создано',
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        'Изменено',
+        auto_now=True,
+    )
+
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
+        ordering = ('-published_at',)
+
+    def __str__(self) -> str:
+        return self.title
