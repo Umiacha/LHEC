2571741dbf7f9637c03df32071ce6b6e2f08ee7d
Umiacha <umiacha@yandex.ru>
2026-08-22 18:53:32 +0300

    Add URLs for future pages.

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,16 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('about/history', views.history, name='history'),
+    path('people/', views.people, name='people'),
+    path('science/', views.science, name='science'),
+    path('science/projects/', views.projects, name='projects'),
+    path('science/publications/', views.publications, name='publications'),
+    path('students/', views.students, name='students'),
+    path('students/materials/', views.materials, name='materials'),
+    path('students/theses/', views.theses, name='theses'),
+    path('news/', views.news, name='news'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('contacts', views.contacts, name='contacts'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,54 @@
-from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 
 
 def home(request: HttpRequest) -> HttpResponse:
-    return render(request, 'core/home.html')
+    return render(request, "core/home.html")
+
+
+def about(request: HttpRequest) -> HttpResponse:
+    return render(request, "core/about.html")
+
+
+def history(request: HttpRequest) -> HttpResponse:
+    return render(request, "core/history.html")
+
+
+def people(request: HttpRequest) -> HttpResponse:
+    return render(request, "core/people.html")
+
+
+def science(request: HttpRequest) -> HttpResponse:
+    return render(request, "core/science.html")
+
+
+def projects(request: HttpRequest) -> HttpResponse:
+    return render(request, "core/projects.html")
+
+
+def publications(request: HttpRequest) -> HttpResponse:
+    return render(request, "core/publications.html")
+
+
+def students(request: HttpRequest) -> HttpResponse:
+    return render(request, "core/students.html")
+
+
+def study_materials(request: HttpRequest) -> HttpResponse:
+    return render(request, "core/study_materials.html")
+
+
+def theses(request: HttpRequest) -> HttpResponse:
+    return render(request, "core/theses.html")
+
+
+def news(request: HttpRequest) -> HttpResponse:
+    return render(request, "core/news.html")
+
+
+def gallery(request: HttpRequest) -> HttpResponse:
+    return render(request, "core/gallery.html")
+
+
+def contacts(request: HttpRequest) -> HttpResponse:
+    return render(request, "core/contacts.html")
