b4a9aaa30baf8218f124927fdf99b61fb99da614
Umiacha <umiacha@yandex.ru>
2026-09-02 00:31:00 +0300

    First structure for file sharing.

--- education/views.py
+++ education/views.py
@@ -1,5 +1,10 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from pathlib import Path
+
+from django.core.exceptions import PermissionDenied
+from django.http import HttpRequest, HttpResponse, FileResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import TeachingMaterial
 
 
 def students(request: HttpRequest) -> HttpResponse:
@@ -8,3 +13,19 @@ def students(request: HttpRequest) -> HttpResponse:
 
 def study_materials(request: HttpRequest) -> HttpResponse:
     return render(request, "core/study_materials.html")
+
+
+def download_materials(request, pk):
+    material = get_object_or_404(
+        TeachingMaterial,
+        pk=pk,
+        is_published=True,
+    )
+    if (material.access_level == TeachingMaterial.AccessLevel.INTERNAL
+        and not request.user.has_perm('education.view_internal_materials')):
+        raise PermissionDenied
+    return FileResponse(
+        material.file.open('rb'),
+        as_attachment=True,
+        filename=Path(material.file.name).name,
+    )
